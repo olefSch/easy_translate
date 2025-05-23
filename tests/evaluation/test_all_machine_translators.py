7065743a3ea1d5451339f3cf93e6a530ceed2609
olefSch <olinho22@gmail.com>
2025-05-23 20:01:45 +0200

    no nllb because of size

--- tests/evaluation/test_all_machine_translators.py
+++ tests/evaluation/test_all_machine_translators.py
@@ -4,7 +4,6 @@ from evaluation.models.base_translator import TranslationError
 from evaluation.models.m2m100_model_translator import M2M100Translator
 from evaluation.models.marianmt_model_translator import MarianTranslator
 from evaluation.models.mBART_50_model_translator import MBartTranslator
-from evaluation.models.nllb_model_translator import NllbTranslator
 
 # Parameterized list of translator classes with appropriate language configurations
 TRANSLATORS = [
@@ -19,16 +18,18 @@ TRANSLATORS = [
     (
         MBartTranslator,
         {"source_lang": "en_XX", "target_lang": "de_DE", "device": "cpu"},
-    ),
-    (
-        NllbTranslator,
-        {
-            "source_lang": "eng_Latn",
-            "target_lang": "deu_Latn",
-            "device": "cpu",
-        },
-    ),
+    ), 
 ]
+
+# NLLB Translator is not included in the test as it requires a specific environment setup
+# (
+#        NllbTranslator,
+#        {
+#            "source_lang": "eng_Latn",
+#            "target_lang": "deu_Latn",
+#            "device": "cpu",
+#        },
+#    ),
 
 
 @pytest.mark.parametrize("cls,kwargs", TRANSLATORS)
