0bdb4824a8d0a382de9966ee4ee1f211b92b5df3
olefSch <olinho22@gmail.com>
2025-05-28 23:16:12 +0200

    test adjustment for ollama

--- tests/unit_tests/conftest.py
+++ tests/unit_tests/conftest.py
@@ -1,5 +1,6 @@
 from typing import Iterable
 import pytest
+import os
 
 from unittest.mock import MagicMock
 from transformers import PreTrainedTokenizer, PreTrainedModel
@@ -11,6 +12,25 @@ from easy_nlp_translate.huggingface_translator_base import (
 )
 from easy_nlp_translate.huggingface_models import MBARTTranslator
 
+
+# --- initilize_translator function
+@pytest.fixture(autouse=True)
+def unregister_ollama_translator_for_test(monkeypatch):
+    """
+    Unregister the 'ollama' translator from the registry for testing purposes.
+    This fixture ensures that the 'ollama' translator is not available during tests,
+    allowing for testing of other translators without interference.
+    """
+    is_ci_environment = os.getenv("CI", "false").lower() in ("true", "1")
+    if is_ci_environment:
+        registry_path = "easy_nlp_translate.initialize.TRANSLATOR_REGISTRY" 
+    
+        module_path, object_name = registry_path.rsplit('.', 1)
+        module = __import__(module_path, fromlist=[object_name])
+        original_registry = getattr(module, object_name)
+
+        if "ollama" in original_registry:
+            monkeypatch.delitem(original_registry, "ollama")
 
 # --- PromptStyle Enum
 @pytest.fixture
